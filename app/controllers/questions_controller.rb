9c1688ecf9475ace115d748944f0d09595c0df1a
Adrian S Muresan & Kit Robinson <pair+adriansmuresan.kitrobinson@devbootcamp.com>
2016-08-10 17:06:14 -0500

    add partial new question form, apply to qlist and profile, basic validation

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -8,3 +8,21 @@ get '/questions/:id' do
   erb :'questions/show'
 end
 
+post '/questions' do
+  if session[:user_id]
+    @question = Question.new(
+      asker_id: session[:user_id],
+      title: params[:title],
+      content: params[:content]
+      )
+    if @question.save
+      redirect '/questions'
+    else
+      @error = "must have both content and title"
+    end
+  else
+    @error = "must be logged in to post!"
+  end
+    @questions = Question.all
+    erb :'/questions/index'
+end
