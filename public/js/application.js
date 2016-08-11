39f9920aac510ce09f4af0e33fc07c226d5ed6a5
Andrea Frank & John Lindner <pair+andreafrank.jalindner@devbootcamp.com>
2016-08-11 15:56:12 -0500

    ajax comments on questions

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,37 @@
 $(document).ready(function() {
+  $('.display-commentbox').on('click', function(event) {
+    event.preventDefault();
+    /* Act on the event */
+    var thisSection = $(this).parent();
+    var formSection = $(thisSection).find('form');
+    $(formSection).toggleClass('hidden');
+    $(this).toggleClass('hidden');
+  });
 
 
+  $('.addcomment').on('submit', function(event) {
+    event.preventDefault();
+    /* Act on the event */
+    $commentForm = $(this);
+    var thisSection = $commentForm.parent();
+    var displayButton = thisSection.find('button');
+    var path = $($commentForm).attr('action');
+    var method = $($commentForm).attr('method');
 
+    $commentForm.toggleClass('hidden');
+    displayButton.toggleClass('hidden');
+
+    var toSend = $(this).serialize();
+
+    $.ajax({
+      url: path,
+      type: method,
+      data: toSend
+    })
+    .done(function(response) {
+      console.log(response);
+      $('.comment-container').append(response);
+    });
+
+  });
 })
